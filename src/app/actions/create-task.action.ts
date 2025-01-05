98efe1807ae0f11326a738f8ede0b5c08827d075
Chrigor <chrigor.edu3@gmail.com>
2025-01-05 19:11:11 -0300

    feat: add list tasks from DB and revalidate path

--- src/app/actions/create-task.action.ts
+++ src/app/actions/create-task.action.ts
@@ -3,6 +3,7 @@
 import { addDoc, collection } from "firebase/firestore";
 import { db } from "../services/firebase";
 import { getServerSession } from "next-auth";
+import { revalidatePath } from "next/cache";
 
 export async function createTask(formData: FormData) {
   try {
@@ -12,11 +13,12 @@ export async function createTask(formData: FormData) {
       public: formData.get("public-task") === "on" ? true : false,
       description: formData.get("description"),
       created: new Date(),
-      user: session?.user?.email
-    }
-    
-    await addDoc(collection(db, "tasks"), task)
+      user: session?.user?.email,
+    };
+
+    await addDoc(collection(db, "tasks"), task);
+    revalidatePath("/dashboard");
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,7 +3,19 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { FaTrash } from "react-icons/fa";
 import { FiShare2 } from "react-icons/fi";
+
 import { createTask } from "../actions/create-task.action";
+
+import {
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
+import { db } from "../services/firebase";
+import { revalidatePath } from "next/cache";
 
 export const metadata: Metadata = {
   title: "Dashboard",
@@ -15,6 +27,15 @@ export default async function Dashboard() {
     redirect("/");
   }
 
+  const myTasksQuery = query(
+    collection(db, "tasks"),
+    orderBy("created", "desc"),
+    where("user", "==", session?.user?.email)
+  );
+
+  const docs = await getDocs(myTasksQuery);
+  let myTasks = docs.docs.map((doc) => doc.data());
+
   return (
     <main className="w-full max-w-[1200px] mx-auto flex flex-col gap-8">
       <section className="w-full">
@@ -22,7 +43,10 @@ export default async function Dashboard() {
           <h1 className="text-slate-200 text-3xl font-bold w-full">
             Qual sua tarefa?
           </h1>
-          <form action={createTask} className="flex flex-col gap-2 w-full">
+          <form
+            action={createTask}
+            className="flex flex-col gap-2 w-full"
+          >
             <textarea
               name="description"
               className="w-full resize-none rounded px-2 py-1 outline-none"
@@ -50,42 +74,30 @@ export default async function Dashboard() {
           Minhas tarefas
         </h1>
         <ul className="flex flex-col gap-5">
-          <li className="w-full">
-            <article className="w-full border flex flex-col gap-2 p-4 rounded bg-slate-200">
-              <div className="flex gap-2">
-                <label className="uppercase bg-cyan-600 text-slate-200 px-2 rounded">
-                  Publico
-                </label>
-                <button className="">
-                  <FiShare2 className="fill-cyan-600" />
-                </button>
-              </div>
-              <div className="flex justify-between gap-2 items-center">
-                <p className="text-md text-slate-900">Minha primeira task</p>
-                <button>
-                  <FaTrash size={24} className="fill-red-800" />
-                </button>
-              </div>
-            </article>
-          </li>
-          <li className="w-full">
-            <article className="w-full border flex flex-col gap-2 p-4 rounded bg-slate-200">
-              <div className="flex gap-2">
-                <label className="uppercase bg-cyan-600 text-slate-200 px-2 rounded">
-                  Publico
-                </label>
-                <button className="">
-                  <FiShare2 className="fill-cyan-600" />
-                </button>
-              </div>
-              <div className="flex justify-between gap-2 items-center">
-                <p className="text-md text-slate-900">Minha primeira task</p>
-                <button>
-                  <FaTrash size={24} className="fill-red-800" />
-                </button>
-              </div>
-            </article>
-          </li>
+          {myTasks.map((task) => {
+            return (
+              <li className="w-full" key={task.description}>
+                <article className="w-full border flex flex-col gap-2 p-4 rounded bg-slate-200">
+                  {task.public && (
+                    <div className="flex gap-2">
+                      <label className="uppercase bg-cyan-600 text-slate-200 px-2 rounded">
+                        Publico
+                      </label>
+                      <button className="">
+                        <FiShare2 className="fill-cyan-600" />
+                      </button>
+                    </div>
+                  )}
+                  <div className="flex justify-between gap-2 items-center">
+                    <p className="text-md text-slate-900">{task.description}</p>
+                    <button>
+                      <FaTrash size={24} className="fill-red-800" />
+                    </button>
+                  </div>
+                </article>
+              </li>
+            );
+          })}
         </ul>
       </section>
     </main>
